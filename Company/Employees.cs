8fdc87d8b2f0382cc5cceef0d4efd46d5f6a2c9f
Блиновс <@223_15>
2018-05-04 21:19:53 +0600

    Add project files.

---
+++ Company/Employees.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company
+{
+    public enum Vacancies { Manager, Boss, Clerk, Salesman}
+
+    public struct Employees
+    {
+        private string FullName_;
+
+        public string FullName
+        {
+            get
+            {
+                return FullName_;
+            }
+            set
+            {
+                FullName_ = value.Replace("<center><b><font size=7>", "").Replace("</font></b></center>", "").Replace("\n", "");
+            }
+        }
+
+        public DateTime StartDate { get; set; }
+
+        public double Salary { get; set; }
+
+        public Vacancies Position { get; set; }
+
+    }
+}
---
+++ Company/Service.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeneratorName;
+
+namespace Company
+{
+    public struct Service
+    {
+        
+        private Random rand;
+
+        public List<Employees> emp;
+
+        public void GenerateEmployee()
+        {
+            emp = new List<Employees>();
+            rand = new Random();
+
+             Generator genr = new Generator();
+
+            for (int i = 0; i < rand.Next(1, 30); i++)
+            {
+                Employees employees = new Employees();
+                employees.FullName = genr.GenerateDefault((Gender)rand.Next(0, 1));
+                employees.StartDate = DateTime.Now.AddMonths((rand.Next(1, 60)) * -1);
+                employees.Salary = rand.Next(30000, 100000) / rand.Next(1, 100);
+                employees.Position = (Vacancies)rand.Next(0, 3);
+                emp.Add(employees);
+            }
+
+
+        }
+
+        public void PrintInfo()
+        {
+            if (emp!= null)
+            {
+
+            }
+            foreach (Employees item in emp)
+            {
+                Console.WriteLine("ФИО: {0}, ({1}) \t {2} {3}",item.FullName, item.StartDate, item.Salary, item.Position);
+            }
+        }
+
+        public void Report1()
+        {
+
+        }
+    }
+}
